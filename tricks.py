20b2f5f7442834622f05d9c734253a48b3421ff1
Yuhao Gu <yhgu2000@outlook.com>
2020-10-16 20:04:04 +0800

    收录一下好用但总是忘用的全局函数

---
+++ tricks.py
@@ -0,0 +1,38 @@
+# 布尔值处理
+all
+any
+
+# 字符串增强
+ascii
+format
+bin
+hex
+oct
+
+# 反射
+dir  # 不带参的使用
+callable
+
+# 动态编译
+compile
+eval
+exec
+
+# 数学
+complex
+divmod
+pow  # 三参数形式
+round
+
+# 迭代
+enumerate
+filter
+map
+reversed
+slice
+
+# 容器
+frozenset
+
+# 低层
+memoryview
